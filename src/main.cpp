e5f1a0f9495c1fea9c56e07f0062f89815648a23
GergelyPiltz <piltzgergely@gmail.com>
2026-01-08 17:10:54 +0100

    Setup

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW", nullptr, nullptr);
+    if (!window)
+        return -1;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
